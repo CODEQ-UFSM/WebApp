206a08ec3fb193838b4745d97849e8dc4aa6ede9
Thiago Reschützegger <thiagoreschutzegger@gmail.com>
2021-01-27 16:19:40 -0300

    Update index.php

--- view/templates/default/index.php
+++ view/templates/default/index.php
@@ -1,62 +1,124 @@
-<section>
-  <div class="container"  id="mais">
-    <div class="row align-items-center">
-      <div class="col-lg-12">
-        <div class="p-5">
-          <h3 class="display-6 fancy">“Out of clutter, find simplicity. From discord, find harmony. In the middle of difficulty lies opportunity.”</h3><h5 style="text-align:right">- <span class="bluemarker shadow">Albert Einstein</span></h5>
-        </div>
-      </div>
-    </div>
-  </div>
-</section>
-<section>
-  <div class="container">
-    <div class="row align-items-center">
-      <div class="col-lg-6 order-lg-2">
-        <div class="p-5">
-          <img class="img-fluid rounded-circle  rounded-circle1" src="<?php echo $this->asset ?>img/circle.png" alt="">
-          <span style="font-size: 20px; color:grey"><center>Logo <b>CodeQ</b></center><span>
-        </div>
-      </div>
-      <div class="col-lg-6 order-lg-1">
-        <div class="p-5">
-          <h2 class="display-4"><span class="bluemarker fancy shadow-lg">Aprenda programação</span></h2>
-          <p>CodeQ é uma plataforma online de ensino de programação e pensamento lógico com foco em aprendizagem baseada em desafios para estudantes da Engenharia Química UFSM, por meio de modelagem e simulação de problemas matemáticos e problemas aplicados ao curso.</p>
-        </div>
-      </div>
-    </div>
-  </div>
-</section>
-
-<section>
-  <div class="container">
-    <div class="row align-items-center">
-      <div class="col-lg-6">
-        <div class="p-5">
-          <img class="img-fluid rounded-circle viravira" src="<?php echo $this->asset ?>img/what.png" alt="">
-        </div>
-      </div>
-      <div class="col-lg-6">
-        <div class="p-5">
-          <h2 class="display-4"><span class="bluemarker fancy shadow-lg">"Lorem ipsum!"</span></h2>
-          <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
-        </div>
-      </div>
-    </div>
-  </div>
-</section>
-
-<section>
-  <div class="container">
-    <div class="row align-items-center">
-      <div class="col-lg-12">
-        <div style="text-align:center" >
-          <a href="<?php echo $this->base_url; ?>Explorar" class="shadow-lg btn btn-primary btn-xl rounded-pill mt-5 js-scroll-trigger degrade">
-            <h3 class="display-4">Explorar</h3>
-          </a>
-        </div>
-      </div>
-    </div>
-  </div>
-</section>
-<br/>
+<section>
+
+  <div class="container"  id="mais">
+
+    <div class="row align-items-center">
+
+      <div class="col-lg-12">
+
+        <div class="p-5">
+
+          <h3 class="display-6 fancy">“Out of clutter, find simplicity. From discord, find harmony. In the middle of difficulty lies opportunity.”</h3><h5 style="text-align:right">- <span class="bluemarker shadow">Albert Einstein</span></h5>
+
+        </div>
+
+      </div>
+
+    </div>
+
+  </div>
+
+</section>
+
+<section>
+
+  <div class="container">
+
+    <div class="row align-items-center">
+
+      <div class="col-lg-6 order-lg-2">
+
+        <div class="p-5">
+
+          <img class="img-fluid rounded-circle  rounded-circle1" src="<?php echo $this->asset ?>img/circle.png" alt="">
+
+          <span style="font-size: 20px; color:grey"><center>Logo <b>CodeQ</b></center><span>
+
+        </div>
+
+      </div>
+
+      <div class="col-lg-6 order-lg-1">
+
+        <div class="p-5">
+
+          <h2 class="display-4"><span class="bluemarker fancy shadow-lg">Aprenda programação</span></h2>
+
+          <p>CodeQ é uma plataforma online de ensino de programação e pensamento lógico com foco em aprendizagem baseada em desafios para estudantes da Engenharia Química UFSM, por meio de modelagem e simulação de problemas matemáticos e problemas aplicados ao curso.</p>
+
+        </div>
+
+      </div>
+
+    </div>
+
+  </div>
+
+</section>
+
+
+
+<section>
+
+  <div class="container">
+
+    <div class="row align-items-center">
+
+      <div class="col-lg-6">
+
+        <div class="p-5">
+
+          <img class="img-fluid rounded-circle viravira" src="<?php echo $this->asset ?>img/what.png" alt="">
+
+        </div>
+
+      </div>
+
+      <div class="col-lg-6">
+
+        <div class="p-5">
+
+          <h2 class="display-4"><span class="bluemarker fancy shadow-lg">"Lorem ipsum!"</span></h2>
+
+          <p>TESTE TESTESed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
+
+        </div>
+
+      </div>
+
+    </div>
+
+  </div>
+
+</section>
+
+
+
+<section>
+
+  <div class="container">
+
+    <div class="row align-items-center">
+
+      <div class="col-lg-12">
+
+        <div style="text-align:center" >
+
+          <a href="<?php echo $this->base_url; ?>Explorar" class="shadow-lg btn btn-primary btn-xl rounded-pill mt-5 js-scroll-trigger degrade">
+
+            <h3 class="display-4">Explorar</h3>
+
+          </a>
+
+        </div>
+
+      </div>
+
+    </div>
+
+  </div>
+
+</section>
+
+<br/>
+
