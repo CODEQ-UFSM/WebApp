f21f832afaf80d25d1ea807ede6e1892d8ac0a5b
Thiago Reschützegger <thiagoreschutzegger@gmail.com>
2021-02-08 12:34:08 -0300

    Update headerindex.php

--- view/templates/default/headerindex.php
+++ view/templates/default/headerindex.php
@@ -1,86 +1,172 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <meta property="og:locale" content="pt_BR">
-
-    <meta property="og:url" content="">
-
-    <meta property="og:title" content="CodeQ">
-    <meta property="og:site_name" content="CodeQ">
-
-    <meta property="og:description" content="Simplificando código">
-
-    <meta property="og:image" content="<?php echo $this->asset; ?>img/circle.png">
-    <meta property="og:image:type" content="image/png">
-    <meta property="og:image:width" content="775">
-    <meta property="og:image:height" content="776">
-
-  <title>CodeQ</title>
-
-  <!-- Bootstrap core CSS -->
-  <link href="<?php echo $this->asset ?>vendor/bootstrap/css/bootstrap.css" rel="stylesheet">
-  <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-
-  <!-- Custom fonts for this template -->
-  <link href="https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900" rel="stylesheet">
-  <link href="https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i" rel="stylesheet">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
-  <!-- Custom styles for this template -->
-  <link href="<?php echo $this->asset ?>css/one-page-wonder.css" rel="stylesheet">
-  <link rel="shortcut icon" href="<?php echo $this->asset ?>img/circle.png">
-  
-
-  <script>
-  function blink_text() {
-    $('.blink').fadeOut(500);
-    $('.blink').fadeIn(500);
-  }
-  setInterval(blink_text, 1500);
-  </script>
-
-</head>
-
-<body>
-
-  <!-- Navigation -->
-  <!--<nav class="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top">
-    <div class="container">
-      <a class="navbar-brand" href="#"><img src="img/LogoWF.png" width="150"></a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarResponsive">
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item">
-            <a class="nav-link" href="#">Sign Up</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Log In</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>-->
-<div id="img-background">
-  <header class="masthead text-center text-white">
-      <div class="masthead-content">
-        <div class="container">
-          <h1 class="masthead-heading mb-0 fancy">CodeQ<span class="blink">&nbsp|</span></h1>
-          <br/><h2 class="masthead-subheading mb-0" style="letter-spacing: 1px; font-size:35px;">Modelagem e simulação simplificada</h2>
-          <br/><a href="#mais" class="btn btn-primary btn-xl rounded-pill mt-5 js-scroll-trigger">Conheça</a><br/><br/>
-        </div>
-      </div>
-      <div class="bg-rotate">
-        <div class="bg-circle-1 bg-circle"></div>
-        <div class="bg-circle-2 bg-circle"></div>
-        <div class="bg-circle-3 bg-circle"></div>
-        <div class="bg-circle-4 bg-circle"></div>
-      </div>
-  </header>
-</div>
+<!DOCTYPE html>
+
+<html lang="en">
+
+
+
+<head>
+
+
+
+  <meta charset="utf-8">
+
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+
+
+    <meta property="og:locale" content="pt_BR">
+
+
+
+    <meta property="og:url" content="">
+
+
+
+    <meta property="og:title" content="CodeQ">
+
+    <meta property="og:site_name" content="CodeQ">
+
+
+
+    <meta property="og:description" content="Simplificando código">
+
+
+
+    <meta property="og:image" content="<?php echo $this->asset; ?>img/circle.png">
+
+    <meta property="og:image:type" content="image/png">
+
+    <meta property="og:image:width" content="775">
+
+    <meta property="og:image:height" content="776">
+
+
+
+  <title>CodeQ</title>
+
+
+
+  <!-- Bootstrap core CSS -->
+
+  <link href="<?php echo $this->asset ?>vendor/bootstrap/css/bootstrap.css" rel="stylesheet">
+
+  <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+
+
+
+  <!-- Custom fonts for this template -->
+
+  <link href="https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+
+  <link href="https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i" rel="stylesheet">
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
+
+  <!-- Custom styles for this template -->
+
+  <link href="<?php echo $this->asset ?>css/one-page-wonder.css" rel="stylesheet">
+
+  <link rel="shortcut icon" href="<?php echo $this->asset ?>img/circle.png">
+
+  
+
+
+
+  <script>
+
+  function blink_text() {
+
+    $('.blink').fadeOut(500);
+
+    $('.blink').fadeIn(500);
+
+  }
+
+  setInterval(blink_text, 1500);
+
+  </script>
+
+
+
+</head>
+
+
+
+<body>
+
+
+
+  <!-- Navigation -->
+
+  <!--<nav class="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top">
+
+    <div class="container">
+
+      <a class="navbar-brand" href="#"><img src="img/LogoWF.png" width="150"></a>
+
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+
+        <span class="navbar-toggler-icon"></span>
+
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+
+        <ul class="navbar-nav ml-auto">
+
+          <li class="nav-item">
+
+            <a class="nav-link" href="#">Sign Up</a>
+
+          </li>
+
+          <li class="nav-item">
+
+            <a class="nav-link" href="#">Log In</a>
+
+          </li>
+
+        </ul>
+
+      </div>
+
+    </div>
+
+  </nav>-->
+
+<div id="img-background">
+
+  <header class="masthead text-center text-white">
+
+      <div class="masthead-content">
+
+        <div class="container">
+
+          <h1 class="masthead-heading mb-0 fancy">Teste<span class="blink">&nbsp|</span></h1>
+
+          <br/><h2 class="masthead-subheading mb-0" style="letter-spacing: 1px; font-size:35px;">Modelagem e simulação simplificada</h2>
+
+          <br/><a href="#mais" class="btn btn-primary btn-xl rounded-pill mt-5 js-scroll-trigger">Conheça</a><br/><br/>
+
+        </div>
+
+      </div>
+
+      <div class="bg-rotate">
+
+        <div class="bg-circle-1 bg-circle"></div>
+
+        <div class="bg-circle-2 bg-circle"></div>
+
+        <div class="bg-circle-3 bg-circle"></div>
+
+        <div class="bg-circle-4 bg-circle"></div>
+
+      </div>
+
+  </header>
+
+</div>
+
