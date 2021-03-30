c2138a859a3d2bbc6c8acad24c39658aec4fafc9
Thiago Reschützegger <thiagoreschutzegger@gmail.com>
2021-03-30 10:19:34 -0300

    Update headerexplorar.php

--- view/templates/default/headerexplorar.php
+++ view/templates/default/headerexplorar.php
@@ -1,78 +1,156 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <meta property="og:locale" content="pt_BR">
-
-    <meta property="og:url" content="">
-
-    <meta property="og:title" content="Projeto">
-    <meta property="og:site_name" content="Projeto">
-
-    <meta property="og:description" content="Simplificando código">
-
-    <meta property="og:image" content="img/circulo.png">
-    <meta property="og:image:type" content="image/png">
-    <meta property="og:image:width" content="775">
-    <meta property="og:image:height" content="776">
-
-  <title>CodeQ</title>
-
-  <!-- Bootstrap core CSS -->
-  <link href="<?php echo $this->asset; ?>vendor/bootstrap/css/bootstrap.css" rel="stylesheet">
-
-  <!-- Custom fonts for this template -->
-  <link href="https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900" rel="stylesheet">
-  <link href="https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i" rel="stylesheet">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
-  <!-- Custom styles for this template -->
-  <link href="<?php echo $this->asset; ?>css/one-page-wonder.css" rel="stylesheet">
-  <link rel="shortcut icon" href="<?php echo $this->asset ?>img/circle.png">
-  
-</head>
-
-<body>
-
-  <!-- Navigation -->
-  <!--<nav class="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top">
-    <div class="container" class="title">
-      <a class="navbar-brand masthead-heading" href="#">CodeQ</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarResponsive">
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item">
-            <a class="nav-link" href="index.html">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Iniciante</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">EQ</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>-->
-<div id="img-background">
-  <header class="masthead text-center text-white">
-      <div class="masthead-content">
-        <div class="container">
-          <h1 class="masthead-heading mb-0 fancy">Explore</h1>
-          <h5 class="" style="font-size: 25px;"><a href="<?php echo $this->base_url; ?>">Home</a> / <a href="<?php echo $this->base_url; ?>Explorar">Explore</a></h5>
-        </div>
-      </div>
-      <div class="bg-rotate">
-        <div class="bg-circle-1 bg-circle"></div>
-        <div class="bg-circle-2 bg-circle"></div>
-        <div class="bg-circle-3 bg-circle"></div>
-        <div class="bg-circle-4 bg-circle"></div>
-      </div>
-  </header>
-</div>
+<!DOCTYPE html>
+
+<html lang="en">
+
+
+
+<head>
+
+
+
+  <meta charset="utf-8">
+
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+
+
+    <meta property="og:locale" content="pt_BR">
+
+
+
+    <meta property="og:url" content="">
+
+
+
+    <meta property="og:title" content="Projeto">
+
+    <meta property="og:site_name" content="Projeto">
+
+
+
+    <meta property="og:description" content="Plataforma online de ensino de programação que busca desafiar estudantes através de modelagens e simulações de problemas matemáticos e aplicados à engenharia.">
+
+
+
+    <meta property="og:image" content="img/circulo.png">
+
+    <meta property="og:image:type" content="image/png">
+
+    <meta property="og:image:width" content="775">
+
+    <meta property="og:image:height" content="776">
+
+
+
+  <title>CodeQ</title>
+
+
+
+  <!-- Bootstrap core CSS -->
+
+  <link href="<?php echo $this->asset; ?>vendor/bootstrap/css/bootstrap.css" rel="stylesheet">
+
+
+
+  <!-- Custom fonts for this template -->
+
+  <link href="https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+
+  <link href="https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i" rel="stylesheet">
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
+
+  <!-- Custom styles for this template -->
+
+  <link href="<?php echo $this->asset; ?>css/one-page-wonder.css" rel="stylesheet">
+
+  <link rel="shortcut icon" href="<?php echo $this->asset ?>img/circle.png">
+
+  
+
+</head>
+
+
+
+<body>
+
+
+
+  <!-- Navigation -->
+
+  <!--<nav class="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top">
+
+    <div class="container" class="title">
+
+      <a class="navbar-brand masthead-heading" href="#">CodeQ</a>
+
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+
+        <span class="navbar-toggler-icon"></span>
+
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+
+        <ul class="navbar-nav ml-auto">
+
+          <li class="nav-item">
+
+            <a class="nav-link" href="index.html">Home</a>
+
+          </li>
+
+          <li class="nav-item">
+
+            <a class="nav-link" href="#">Iniciante</a>
+
+          </li>
+
+          <li class="nav-item">
+
+            <a class="nav-link" href="#">EQ</a>
+
+          </li>
+
+        </ul>
+
+      </div>
+
+    </div>
+
+  </nav>-->
+
+<div id="img-background">
+
+  <header class="masthead text-center text-white">
+
+      <div class="masthead-content">
+
+        <div class="container">
+
+          <h1 class="masthead-heading mb-0 fancy">Explore</h1>
+
+          <h5 class="" style="font-size: 25px;"><a href="<?php echo $this->base_url; ?>">Home</a> / <a href="<?php echo $this->base_url; ?>Explorar">Explore</a></h5>
+
+        </div>
+
+      </div>
+
+      <div class="bg-rotate">
+
+        <div class="bg-circle-1 bg-circle"></div>
+
+        <div class="bg-circle-2 bg-circle"></div>
+
+        <div class="bg-circle-3 bg-circle"></div>
+
+        <div class="bg-circle-4 bg-circle"></div>
+
+      </div>
+
+  </header>
+
+</div>
+
