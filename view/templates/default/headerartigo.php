b6aacf6e6c6bf8d360bfdd11a63ce267df59f7f1
Thiago Reschützegger <thiagoreschutzegger@gmail.com>
2021-04-20 22:48:01 -0300

    google analytics tag

--- view/templates/default/headerartigo.php
+++ view/templates/default/headerartigo.php
@@ -12,7 +12,13 @@
 
 <head>
 
-
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NJLNHTG');</script>
+<!-- End Google Tag Manager -->
 
     <meta charset="utf-8">
 
@@ -481,13 +487,6 @@
 
 <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
 
-<!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-NJLNHTG');</script>
-<!-- End Google Tag Manager -->
 </head>
 
 
--- view/templates/default/headerexplorar.php
+++ view/templates/default/headerexplorar.php
@@ -6,7 +6,13 @@
 
 <head>
 
-
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NJLNHTG');</script>
+<!-- End Google Tag Manager -->
 
   <meta charset="utf-8">
 
@@ -68,14 +74,6 @@
 
   <link rel="shortcut icon" href="<?php echo $this->asset ?>img/circle.png">
 
-
-<!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-NJLNHTG');</script>
-<!-- End Google Tag Manager -->
 </head>
 
 
